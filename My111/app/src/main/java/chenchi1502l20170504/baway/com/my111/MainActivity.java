1da150c620451f69ffbef734b4bb19e1aa943b69
keai90008000 <415780449@qq.com>
2017-05-08 17:18:04 +0800

    frish

---
+++ My111/app/src/main/java/chenchi1502l20170504/baway/com/my111/MainActivity.java
@@ -0,0 +1,13 @@
+package chenchi1502l20170504.baway.com.my111;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
